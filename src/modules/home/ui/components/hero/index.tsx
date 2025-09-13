e94b2839b3db50c78d7b5c145651b2e3e4db95f4
theoldonee <rouvaf123@gmail.com>
2025-09-13 16:12:49 +0400

    feat(hero): create hero prop

---
+++ src/modules/home/ui/components/hero/index.tsx
@@ -0,0 +1,11 @@
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"]});
+export const Hero = () =>{
+    return(
+        <div className="pt-16 w-full h-screen bg-[url('/hero-bg.jpg')] bg-cover bg-center flex items-center justify-center gap-4 flex-col text-white">
+            <span className={`${raleway.className} text-6xl`}>HI, I'M FAVOUR</span>
+            <span>Developer</span>
+        </div>
+    );
+}
