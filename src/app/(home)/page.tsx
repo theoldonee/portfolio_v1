3bdaa8f8bb2469c4dfae0b8d36f9d737e800a90f
theoldonee <rouvaf123@gmail.com>
2025-09-13 16:13:27 +0400

    feat(/app/(home/)page): cretae Home

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,17 @@
+import { Hero } from "@/modules/home/ui/components/hero/index";
+
+export default function Home() {
+  return (
+    <div>
+      {/* Hero */}
+      <Hero/>
+      {/* About */}
+      {/* Interest */}
+      {/* Technologies */}
+      {/* Cv */}
+      {/* What I built */}
+      {/* Get in touch */}
+      {/* Footer */}
+    </div>
+  );
+}
