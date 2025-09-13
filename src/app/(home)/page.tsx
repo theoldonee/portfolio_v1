3b77ef67990361507d643674a9065642f2d05978
theoldonee <rouvaf123@gmail.com>
2025-09-13 22:54:25 +0400

    feat((home).page.tsx): add about me section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { ExperienceSection } from "@/modules/genreral/ui/components/experience-section/index";
 import { Hero } from "@/modules/home/ui/components/hero/index";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -6,6 +9,26 @@ export default function Home() {
       {/* Hero */}
       <Hero/>
       {/* About */}
+      <div id="About" className="w-full h-[calc(100vh-200px)] bg-white flex flex-col items-center justify-center gap-3.5">
+        <span
+          className="text-5xl"
+        >ABOUT ME</span>
+        <Image
+          width={300}
+          height={300}
+          alt="me"
+          src="/hero-bg.jpg"
+          className="w-52 h-52 border rounded-full"
+        />
+        <span className="w-200">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis suscipit, at recusandae facere ipsum officiis aspernatur, dicta velit illum similique voluptate corrupti repellendus ea dolores itaque, harum commodi laudantium officia.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolore maiores expedita obcaecati error provident fugiat nostrum esse. Magni, eligendi? Incidunt aliquam exercitationem ab libero est quia eius minus officia?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ex eligendi porro eveniet repellendus asperiores doloremque harum! Sapiente dolores maxime saepe nobis assumenda, itaque sequi unde pariatur vitae odit quos!
+        </span> 
+        <Button>
+          More About Me
+        </Button>
+      </div>
       {/* Interest */}
       {/* Technologies */}
       {/* Cv */}
