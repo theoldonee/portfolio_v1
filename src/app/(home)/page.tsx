48187468702ac3634d331e016570a953b4e128e6
theoldonee <rouvaf123@gmail.com>
2025-09-23 00:41:44 +0400

    feat((home)/page.tsx): add Interest section, Experience section and cv section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { ExperienceSection } from "@/modules/genreral/ui/components/experience-section/index";
 import { Hero } from "@/modules/home/ui/components/hero/index";
+
 import Image from "next/image";
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"]});
 
 export default function Home() {
   return (
@@ -11,7 +15,7 @@ export default function Home() {
       {/* About */}
       <div id="About" className="w-full h-[calc(100vh-200px)] bg-white flex flex-col items-center justify-center gap-3.5">
         <span
-          className="text-5xl"
+          className={`text-4xl ${raleway.className}`}
         >ABOUT ME</span>
         <Image
           width={300}
@@ -25,13 +29,31 @@ export default function Home() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolore maiores expedita obcaecati error provident fugiat nostrum esse. Magni, eligendi? Incidunt aliquam exercitationem ab libero est quia eius minus officia?
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ex eligendi porro eveniet repellendus asperiores doloremque harum! Sapiente dolores maxime saepe nobis assumenda, itaque sequi unde pariatur vitae odit quos!
         </span> 
+        {/* TODO: link to me page */}
         <Button>
           More About Me
         </Button>
       </div>
       {/* Interest */}
+      <div id="Passion" className="w-full h-[calc(100vh-200px)] flex items-center justify-center gap-3.5 bg-amber-300">
+        
+      </div>
       {/* Technologies */}
+      <ExperienceSection/>
       {/* Cv */}
+      <div className="w-full bg-[url('/resume-backdrop.jpg')] bg-fixed bg-center bg-cover relative">
+        <div className="w-full h-full absolute top-0 left-0 bg-black opacity-40 z-0">
+
+        </div>
+        <div className="relative z-10 w-full h-72 flex flex-col items-center justify-center gap-4">
+          <span className={`text-4xl ${raleway.className} text-white`}>CHECK OUT MY RESUME</span>
+          {/* TODO: add cv download on click */}
+          <Button>
+            Grab A Copy
+          </Button>
+        </div>
+        
+      </div>
       {/* What I built */}
       {/* Get in touch */}
       {/* Footer */}
