98d36b7089d3dd367ef9d18502bc0507d18dca7f
theoldonee <rouvaf123@gmail.com>
2025-09-23 11:40:37 +0400

    feat(src/layout.tsx): add cloud flare cdn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className=" scroll-smooth">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
