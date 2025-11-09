5ee71fb99bcd52593e20126a62d349326147134e
theoldonee <rouvaf123@gmail.com>
2025-11-09 21:39:58 +0400

    feat(hero/index.tsx): add animated-self-carousel

--- src/modules/home/ui/components/hero/index.tsx
+++ src/modules/home/ui/components/hero/index.tsx
@@ -1,12 +1,12 @@
-"use client";
 import { Raleway } from "next/font/google";
+import { AnimatedSelfCarousel } from "./animated-self-carousel";
 
 const raleway = Raleway({ subsets: ["latin"]});
 export const Hero = () =>{
     return(
         <div className="pt-16 w-full h-screen bg-[url('/hero-bg.jpg')] bg-cover bg-center flex items-center justify-center gap-4 flex-col text-white">
             <span className={`${raleway.className} text-6xl`}>HI, I'M FAVOUR</span>
-            <span>Developer</span>
+            <AnimatedSelfCarousel/>
         </div>
     );
 }
